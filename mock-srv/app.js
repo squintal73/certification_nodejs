11d8bcc080d214c53cbc6b8f43df4c879db9515c
squintal73 <sidnei_quintal@yahoo.com.br>
2023-06-06 21:12:23 -0300

    modulo-01

---
+++ mock-srv/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const cors = require('@fastify/cors')
+const path = require('path')
+const AutoLoad = require('@fastify/autoload')
+
+
+
+module.exports = async function (fastify, opts) {
+  fastify.register(cors)
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
+
